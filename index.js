c5f48a679603301746fec676ad0eb7dd9de9daee
H <38281581+MHuiG@users.noreply.github.com>
2025-10-24 06:37:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -37,3 +37,13 @@ hexo.extend.renderer.register('mkdn', 'html', renderer, true);
 hexo.extend.renderer.register('mdwn', 'html', renderer, true);
 hexo.extend.renderer.register('mdtxt', 'html', renderer, true);
 hexo.extend.renderer.register('mdtext', 'html', renderer, true);
+
+
+
+
+const config = require('hexo-xmath/lib/config')(hexo);
+const {filter_md, filter_math,  render} = require('hexo-xmath/lib/process');
+
+hexo.extend.filter.register('before_post_render', data => {filter_md(config,data)}, 5);
+hexo.extend.filter.register('after_post_render', async(data) => {await filter_math(config,data)}, 5);
+hexo.extend.filter.register('after_render:html', (html, { page }) => {return render(config,html,page)});
